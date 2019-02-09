cbeb9cf60602d976d5c74b6a826653f66f5cc2d6
nzwan <36454311+nzwan@users.noreply.github.com>
2019-02-09 13:56:08 -0500

    added registration

---
+++ photomanager/forms/registration_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class RegistrationForm(FlaskForm):
+	name = StringField('name', validators = [DataRequired()])
+	username = StringField('Username', validators = [DataRequired()])
+	password = PasswordField('Password', validators =[DataRequired()])
+    
+	confirm_password = PasswordField('Password', validators =[DataRequired()])
+    
+	registration_button = SubmitField('Register')
