102eb07a4627cc8362b3f5e6c2a6a6f36f953754
Aaron Bolyard <bolyarda9820@student.faytechcc.edu>
2019-02-25 15:37:55 -0500

    Prettified code.

--- photomanager/views/photo/delete.py
+++ photomanager/views/photo/delete.py
@@ -1,23 +1,14 @@
 from flask import (
         Blueprint, flash, g, redirect, render_template,
         request, session, url_for
-        )
-
-from flasklogin import (
+)
+from flask_login import (
     current_user, login_required
 )
 
 from photomanager.common.photo import delete_photo
-
-
-
-
-
-bp = Blueprint('home', __name__, url_prefix= '/')
-
-from photomanager.views.phot.bp import bp
-from photomanager.common.database import get_database
-from photomanager.model.user import user
+from photomanager.model.photo import Photo
+from photomanager.views.photo.bp import bp
 
 @bp.route('/delete/confirm/<int:photo_id>')
 @login_required
@@ -26,7 +17,7 @@ def confirm_delete(photo_id):
     if photo.user_id != current_user.get_user().id:
         return abort(404)
     else:
-        return render_template('photo/confirm_delete.html", photo=photo')
+        return render_template("photo/confirm_delete.html", photo=photo)
 
 @bp.route('/delete/perform/<int:photo_id>')
 @login_required
@@ -37,4 +28,3 @@ def perform_delete(photo_id):
     else:
         delete_photo(photo)
         return redirect(url_for("photos.view"))
-        
